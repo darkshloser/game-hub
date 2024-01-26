c417999ba6ddf52efe39bd4d130138e3ca4252c4
Dobromir Kovachev <dobromir.mail@gmail.com>
2024-01-26 11:43:31 +0200

    Modify api-client access permissions

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '121f38844d374ffbb7fbf492d3c8d51e'
+        key: ''
     }
 })
 
